2442cca1d5c819852effbdbd2d24f922b1cec76c
psarma89 <priyam.sarma@gmail.com>
2017-10-10 10:52:52 -0400

    Add nyc pigeon organizer solution file

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  output_hash = {}
+  pigeons = data[:gender].values.flatten!
+  pigeons.each{|pigeon| output_hash[pigeon]={}}
+  data.keys.each{|key| output_hash.each{|pigeon,value| value[key] = []}}
+
+  data.each do |key,value|
+    value.each do |key1,value1|
+      value1.each{|pigeon| output_hash[pigeon][key] << key1.to_s}
+    end
+  end
+
+  output_hash
+
 end
