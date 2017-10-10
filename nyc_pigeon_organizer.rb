7d435cc5ea91bcbe40e1566960c7130f7adbd68f
Seth Barden <sethbarden@gmail.com>
2017-10-10 20:39:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  clean = {}
+  data.each do |attr_key, hash|
+    hash.each do |desc_key, name_array|
+      name_array.each do |name|
+        if clean[name] == nil
+          clean[name] = {}
+        end
+        if clean[name][attr_key] == nil
+          clean[name][attr_key] = []
+        end
+        clean[name][attr_key] << desc_key.to_s
+      end
+    end
+  end
+  clean
 end
