ebde4c29f5e415e19cd2dc91b60e46d47e2b7b61
Jake <jakemacnaughton@gmail.com>
2017-10-12 22:38:56 -0400

    Done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = Hash.new(0)
+  data.each do |c_g_l_key, hash|
+    hash.each do |key, value_array|
+      value_array.each do |name|
+        if pigeon_list[name] == 0
+          pigeon_list[name] = Hash.new(0)
+        end
+        if pigeon_list[name][c_g_l_key] == 0
+          pigeon_list[name][c_g_l_key] = []
+        end
+        pigeon_list[name][c_g_l_key] << key.to_s
+      end
+    end
+  end
+  pigeon_list
 end
