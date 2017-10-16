c0580c792fb74e02e2914015cf9a297868246dac
George Gore <georgelgore@gmail.com>
2017-10-16 21:00:43 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pidgeon_list = {}
+
+  data.each do |category, hash|
+    hash.each do |info, pidgeons|
+      pidgeons.each do |pidgeon|
+        pidgeon_list[pidgeon] = {:color => [], :gender => [], :lives => []}
+      # {:color => [], :gender => [], :lives => []}
+      end
+    end
+  end
+
+
+  data.each do |category, hash|
+    hash.each do |info, pidgeons|
+      pidgeon_list.each do |name, info_hash|
+
+          info_hash[category] << info.to_s if pidgeons.include?(name)
+
+      end
+    end
+  end
+
+  # pidgeons.each do |pidgeon|
+  #   if pidgeons.include?(pidgeon_list[pidgeon])
+  #     pidgeon_list[pidgeon][category] << info
+  #   end
+  # end
+
+  pidgeon_list
 end
