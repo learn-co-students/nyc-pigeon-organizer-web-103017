8dc94f0432658e7f932b19cd5d0c6b5fec9a0217
Dan O'Dea <dansbands@hotmail.com>
2017-10-19 20:14:21 +0000

    done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |trait, options|#:color
+    options.each do |option, names|#:purple
+      names.each do |name|#"Theo"
+        pigeon_list[name] = {} unless pigeon_list[name]
+        pigeon_list[name][trait] = [] unless pigeon_list[name][trait]
+        pigeon_list[name][trait] << option.to_s
+        # binding.pry
+      end
+    end
+  end
+  pigeon_list
 end
