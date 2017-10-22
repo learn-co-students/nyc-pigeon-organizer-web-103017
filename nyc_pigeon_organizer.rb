0fc201a6b70e8dbcd81ea13a549ce3696d31ae6b
lierica <ericaaa.li@gmail.com>
2017-10-22 01:17:33 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    organized_pigeons = {}
+    data.each do |category, values|
+        values.each do |key, array|
+            array.each do |name|
+                organized_pigeons[name] ||= {}
+                organized_pigeons[name][category] ||= []
+                organized_pigeons[name][category] << key.to_s
+            end
+        end
+    end
+    organized_pigeons
 end
